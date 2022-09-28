91a689514e6a32418df25c309b634a1c6c10bb88
hharieta <L20530228@cancun.tecnm.mx>
2022-09-28 16:27:26 -0500

    990 - agregadas más opciones al menú

--- 990_Python_OS_Module/menuOS.py
+++ 990_Python_OS_Module/menuOS.py
@@ -2,22 +2,32 @@ import os
 import asyncio
 import sys
 from sys import stderr, stdout
-
-"""
-Menú para interactuar con el módulo OS
-"""
+import re
 
 def opciones(opcion):
     match opcion:
         case "1":
             print(os.name)
-            
+            print("\n\033[1;32m"+"[*] \033[2;36m"+"nombre del módulo dependiente del sistema operativo importado"+'\033[0;m')
         case "2":
-            # imprime todas las variables de entorno del sistma
+            print(os.uname())
+            print("\n\033[1;32m"+"[*] \033[2;36m"+"información que identifica el sistema operativo actual"+'\033[0;m')
+        case "3":
+            print(os.ctermid())
+            print("\n\033[1;32m"+"[*] \033[2;36m"+"terminal de control del proceso"+'\033[0;m')
+        case "4":
             # intente os.environ['VARIABLE_ENTORNO'] 
             print(os.environ)
-            print("\033[1;32m"+"[*] \033[2;36m"+"variables de entorno del sistema"+'\033[0;m')
+            print("\n\033[1;32m [*]" + " \033[2;36m variables de entorno del sistema"+'\033[0;m')
+        case "5":
+            # Bytes version of os.environ
+            print(os.environb)
+            print("\n\033[1;32m [*]" + " \033[2;36m variables de entorno del sistema en bytes"+'\033[0;m')
+            print("\033[1;33m [*]" + " \033[1;33m environb solo está disponible si support_bytes_environ es" + "\033[1;35m True" +'\033[0;m')
+        case "6":
+            print(os.chdir("/home/gatovsky"))
         case _:
+            print("\n\033[1;31m [*]" + " \033[1;33m Opción no válida"+'\033[0;m')
             exit
         
 
@@ -27,17 +37,28 @@ def menu():
         os.system('clear')
 
         print("""
-        Uso: """ + os.path.basename(__file__) + """ [OPCIONES]
-        Puede elegir entre las siguientes opciones :
-            --1) os.name
-            --2) os.environ
-            --0) exit
+        **************************************
+        Menú para interactuar con el módulo OS
+        **************************************
+
+        Opciones en """ + os.path.splitext(os.path.basename(__file__))[0] + """
+        
+        Parámetros de procesos:
+            1) os.name
+            2) os.uname()
+            3) os.ctermid()
+            4) os.environ
+            5) os.environb
+            6) os.chdir
+            0) exit
         """)
         opcion = input("\n> ")
 
         if opcion == "0": break
-        else:
+        elif opcion.isnumeric():
             opciones(opcion)
+        else:
+            print("\n\033[1;31m [*]" + " \033[1;33m Entrada no numérica"+'\033[0;m')
 
 
 def main():
