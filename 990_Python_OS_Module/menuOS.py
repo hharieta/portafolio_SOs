05dcca5bc9def20614fde200b7a469d271636b86
hharieta <L20530228@cancun.tecnm.mx>
2022-10-02 16:43:55 -0500

    Corregido os.chdir(path) que retornaba None

--- 990_Python_OS_Module/menuOS.py
+++ 990_Python_OS_Module/menuOS.py
@@ -25,7 +25,9 @@ def opciones(opcion):
             print("\n\033[1;32m [*]" + " \033[2;36m variables de entorno del sistema en bytes"+'\033[0;m')
             print("\033[1;33m [*]" + " \033[1;33m environb solo está disponible si support_bytes_environ es" + "\033[1;35m True" +'\033[0;m')
         case "6":
-            print(os.chdir("/home/gatovsky"))
+            os.chdir("/home/gatovsky")
+            print(os.getcwd())
+
         case _:
             print("\n\033[1;31m [*]" + " \033[1;33m Opción no válida"+'\033[0;m')
             exit
