f0ed46bd2a978bd6a37d14bdf0d0f3661f65ea8d
hharieta <L20530228@cancun.tecnm.mx>
2022-10-07 14:12:33 -0500

    994 - script 13: modificada la expresión de until

--- 994-992_Menu_ASCIInema/scripts/Unitl.sh
+++ 994-992_Menu_ASCIInema/scripts/Unitl.sh
@@ -5,9 +5,9 @@
 echo "Estructura until"
 
 cont=15
-until (test ${cont} -lt 0)
+until [ $cont -lt 0 ]
 do
-  cont=`expr $cont -1`
+  cont=`expr $cont - 1`
   echo "Valor del contador: ${cont}"
 done
 
